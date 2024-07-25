28a997c1c3525617542dde99f5e1709a52c40ce3
Saeed Altout <saeedaltoutpro@gmail.com>
2024-07-25 13:12:09 +0300

    update and org files

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
-import cors from "cors";
+
 import { router } from "./routes";
 
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
